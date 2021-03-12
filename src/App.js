e3c15b2724083dd4dd3ff9a423233bdccf8ed872
syl20ko <sylvain.caron@baliztic.com>
2021-03-12 01:21:11 +0100

    import my blender project into react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Sky } from "@react-three/drei";
+import Farm from "./Farmthreejs";
+import Boy from "./Boy";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [-30, 5, 10], fov: 90 }}>
+      <ambientLight />
+      <directionalLight intensity={1} />
+      <ambientLight intensity={0.2} />
+      <Suspense fallback={null}>
+        <Boy position={[-15, -5, 0]} animation={true} />
+        <Farm position={[-5, 0, 0]} />
+        <Sky
+          distance={450000} // Camera distance (default=450000)
+          sunPosition={[0, 1, 0]} // Sun position normal (defaults to inclination and azimuth if not set)
+          inclination={0} // Sun elevation angle from 0 to 1 (default=0)
+          azimuth={0.25} // Sun rotation around the Y axis from 0 to 1 (default=0.25)
+        />
+      </Suspense>
+      <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={0} />
+    </Canvas>
   );
 }
 
